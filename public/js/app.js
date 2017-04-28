cf8bd02a1f3f81b7f5c6f92af7ab1bbb0af1a084
surfing-girl <joanna.demecka@gmail.com>
2017-04-28 22:35:42 +0100

    Hardcoded albums printed to the screen.

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,9 @@
-$.getJSON('/js/data.json', (data) => {
-  let info = '<ul>';
-  $.each(data, (index, lover) => {
-    info += '<li>' + lover.name + ': ' + lover.age + ' <3 </li>'
+$.getJSON('https://api.spotify.com/v1/search?q=muse&type=album', (data) => {
+  let info = '';
+  $.each(data.albums.items, (index, album) => {
+    info += '<li><div class="album-wrap"><img class="album-art" src="' + album.images[1].url + '"/></div>'
+    info += '<span class="album-title">' + album.name + '</span>'
+    info += '<span class="album-artist">' + album.artists[0].name + '</span></li>';
   });
-  info += '</ul>';
   $('#albums').html(info);
 });
