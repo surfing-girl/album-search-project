8c977586a7bd1a36051c427ee61764a911d7193e
surfing-girl <joanna.demecka@gmail.com>
2017-05-04 13:11:29 +0100

    Project meets expectation

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,17 @@
-$.getJSON('https://api.spotify.com/v1/search?q=muse&type=album', (data) => {
-  let info = '';
-  $.each(data.albums.items, (index, album) => {
-    info += '<li><div class="album-wrap"><img class="album-art" src="' + album.images[1].url + '"/></div>'
-    info += '<span class="album-title">' + album.name + '</span>'
-    info += '<span class="album-artist">' + album.artists[0].name + '</span></li>';
+$('form').submit((e) => {
+  let query = document.getElementById('search').value;
+  e.preventDefault();
+  $.getJSON('https://api.spotify.com/v1/search?q=' + query + '&type=album', (data) => {
+    let info = '';
+    if (data.albums.items.length > 0) {
+      $.each(data.albums.items, (index, album) => {
+        info += '<li><div class="album-wrap"><img class="album-art" src="' + album.images[0].url + '"/></div>'
+        info += '<span class="album-title">' + album.name + '</span>'
+        info += '<span class="album-artist">' + album.artists[0].name + '</span></li>';
+      });
+    } else {
+      info = "<li class='no-albums desc'><i class='material-icons icon-help'>help_outline</i>No albums found that match: " + query + ".</li>"
+    }
+    $('#albums').html(info);
   });
-  $('#albums').html(info);
 });
